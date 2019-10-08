5266a38aa80a2cbc9dfd52f80cf759ec0c0f3c88
Jori Peterson <joripeterson@gmail.com>
2019-10-08 15:13:17 -0600

    Dry up header code, add module export

--- routes/api/v1/foods.js
+++ routes/api/v1/foods.js
@@ -3,13 +3,15 @@ var router = express.Router();
 var Food = require('../../../models').Food;
 
 router.get("/", function(req, res, next) {
+  res.setHeader("Content-type", "application/json")
+
   Food.findAll()
   .then(foods => {
-    res.setHeader("Content-Type", "application/json");
     res.status(200).send(JSON.stringify(foods));
   })
   .catch(error => {
-    res.setHeader("Content-Type", "application/json");
     res.status(500).send({error})
   });
 });
+
+module.exports = router;
