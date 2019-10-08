b0abc8fa0d72140083197700ca563e99f1dbca71
Aurie <Myrdden42@gmail.com>
2019-10-08 14:55:05 -0600

    Created GET Single Food by ID

---
+++ routes/api/v1/foods.js
@@ -0,0 +1,13 @@
+var router = require('express').Router();
+var Food = require('../../../models').Food;
+
+router.get('/foods/:id', (req, res) => {
+  res.setHeader('Content-Type', 'application/json');
+  Food.findOne({where: {id: req.params.id}})
+  .then(food => {
+    res.status(200).send(JSON.stringify(food));
+  })
+  .catch(error => {
+    res.status(404).send();
+  })
+});
